6f852f02526582feda39434c0704e86b24f04a90
Bruce Aldridge <bruce@incode.co.nz>
2013-07-19 20:53:44 +1200

    adding in example.php

---
+++ example.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'src/VendAPI/VendAPI.php';
+
+$vend = new VendAPI\VendAPI('https://shopname.vendhq.com','username','password');
+
+$products = $vend->getProducts();
+
+$donut = $sale = new \VendAPI\VendProduct(null, $vend);
+$donut->handle = 'donut01';
+$donut->sku = '343434343';
+$donut->retail_price = 2.99;
+$donut->name = 'Donut w/ Sprinkles';
+$donut->save();
+echo 'Donut product id is '.$donut->id;
