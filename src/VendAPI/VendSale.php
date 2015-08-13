61bea52508f79ea3bd986647521b9c21bfca49d3
Bob Brown <bob@turboweb.co.nz>
2015-08-13 22:20:17 +1200

    Added getCustomer and getProducts to VendSale object

--- src/VendAPI/VendSale.php
+++ src/VendAPI/VendSale.php
@@ -19,6 +19,34 @@ namespace VendAPI;
 class VendSale extends VendObject
 {
     /**
+     * Gets the customer associated with the sale
+     * @return VendCustomer
+     */
+    public function getCustomer()
+    {
+        $customers = $this->vend->getCustomers(array('id' => $this->customer_id));
+        if(empty($customers))
+        {
+            throw new Exception( 'Unable to find customer ' . $this->customer_id . ' for sale' );
+        }
+        return $customers[0];
+    }
+
+    /**
+     * Gets the products associated with the sale - return array of [VendProduct]s
+     * @return array
+     */
+    public function getProducts()
+    {
+        $products = array();
+        foreach( $this->register_sale_products as $product )
+        {
+            $products[] = $this->vend->getProduct($product->product_id);
+        }
+        return $products;
+    }
+
+    /**
      * will create/update the user using the vend api and this object will be updated
      * @return null
      */
